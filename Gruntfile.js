193170aa6ff3580849f4b03913602ce0007e2e7f
ChrisBAshton <chrisashtonweb@gmail.com>
2014-12-17 10:20:57 +0000

    added images for social media category entry

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd:    'assets/' + webbies_year,
-                    src:    ['img/*'], 
+                    src:    ['img/**'], 
                     dest:   'english'
                 }]
             },
